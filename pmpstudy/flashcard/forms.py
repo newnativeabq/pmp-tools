87e276e3c045a21280517f76c64be071794d8071
Vincent B <vincent@itouchup.org>
2019-01-19 11:57:30 -0700

    resettign directories after collect static failed with some changes to flashcard.css

--- pmpstudy/flashcard/forms.py
+++ pmpstudy/flashcard/forms.py
@@ -14,8 +14,8 @@ class FlashCardForm(forms.ModelForm):
         ##  Adding widgets for later styling
         widgets = {
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
-            'frontface':forms.Textarea(attrs={'class':'frontface'}),
-            'backface':forms.Textarea(attrs={'class':'backface'}),
+            'frontface':forms.Textarea(attrs={'class':'frontface medium-editor-textarea'}),
+            'backface':forms.Textarea(attrs={'class':'backface medium-editor-textarea'}),
             'know_level':forms.NumberInput(attrs={'class':'know_level'}),
             'activated':forms.CheckboxInput(attrs={'class':'activated'}),
         }
